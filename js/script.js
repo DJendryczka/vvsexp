fe40bfb03f691ace6a7157d9d81c4b470fd9f366
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-08-05 18:55:41 +0200

    Add basic html and navbar

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* Navbar */
+const navSlide = () => {
+  const burger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animaton = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    burger.classList.toggle('toggle');
+  });
+
+  nav.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+  });
+};
+
+navSlide();
