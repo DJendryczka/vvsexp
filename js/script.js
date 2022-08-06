a6ab83da3899178a390d9c69e0c8c71cacbf4f4b
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-08-06 10:02:00 +0200

    Add style for prev.

--- js/script.js
+++ js/script.js
@@ -26,3 +26,54 @@ const navSlide = () => {
 };
 
 navSlide();
+/* Carousel */
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
+const timeOut = setTimeout('autoSlide', 1000)
+console.log(slides);
+
+
+document
+  .getElementById('carousel-button-next')
+  .addEventListener('click', moveToNextSlide);
+document
+  .getElementById('carousel-button-prev')
+  .addEventListener('click', moveToPrevSlide);
+  // Next on is to made carousel automaticly go to next slide...
+function hideAllSlides() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel-item-visible');
+    slide.classList.add('carousel-item-hidden');
+  }
+}
+ function autoSlide(){
+    hideAllSlides()
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  slides[slidePosition].classList.add('carousel-item-visible');
+  setTimeout(autoSlide, 5000);
+ }
+ 
+function moveToNextSlide() {
+    hideAllSlides()
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  slides[slidePosition].classList.add('carousel-item-visible');
+}
+function moveToPrevSlide() {
+    hideAllSlides()
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+  slides[slidePosition].classList.add('carousel-item-visible');
+}
+window.onload=autoSlide()
